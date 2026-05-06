8c50cfb33f0af1a56d4761a20a4b214bb289dc24
yuki <y_toi@mac.com>
2026-05-06 18:37:22 +0900

    added opencv in setup to use camera

--- spike/spike_env_setup_linux.sh
+++ spike/spike_env_setup_linux.sh
@@ -1,9 +1,9 @@
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y install ruby
+sudo apt -y install ruby libzbar0 libzbar-dev python3-pip
 sudo gem install shell
 python -m venv ../tools/python
-../tools/python/bin/pip install pyusb
+../tools/python/bin/pip install pyusb opencv-python pyzbar
 sudo apt -y install minicom
 echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0483\",ATTRS{idProduct}==\"5740\", SYMLINK+=\"USB_SPIKE\",MODE=\"0666\"" > /tmp/serial_temp_rule
 sudo mv /tmp/serial_temp_rule /etc/udev/rules.d/99-serial.rules
